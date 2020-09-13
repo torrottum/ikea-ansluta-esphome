2b775ce8f0b44694a406eb62c40e67cfb4fc623d
Tor Røttum <tor@rottum.me>
2020-09-13 12:32:55 +0200

    get remote addr from topic

--- ansluta_light.h
+++ ansluta_light.h
@@ -13,8 +13,28 @@ public:
   void loop() {
   }
 
+  char hexdigit(char hex)
+  {
+    return (hex <= '9') ? hex - '0' : toupper(hex) - 'A' + 10;
+  }
+
+  char hexbyte(const char *hex)
+  {
+    return (hexdigit(*hex) << 4) | hexdigit(*(hex + 1));
+  }
+
+  void getRemoteAddressFromTopic(const std::string &topic, char &addrA, char &addrB)
+  {
+    addrA = hexbyte(topic.substr(topic_prefix.length()+1, 2).c_str());
+    addrB = hexbyte(topic.substr(topic_prefix.length()+3, 2).c_str());
+  }
+
   void on_set(const std::string &topic, const std::string &payload)
   {
     ESP_LOGD("ansluta", "Topic: '%s', Payload: '%s'", topic.c_str(), payload.c_str());
+    char addrA;
+    char addrB;
+    getRemoteAddressFromTopic(topic, addrA, addrB);
+    ESP_LOGD("ansluta", "Remote address: %#2x %#2x", addrA, addrB);
   }
 };
