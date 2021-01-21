92c05014a310a098242a595344644485d2802153
Tor Røttum <tor@rottum.me>
2021-01-21 21:12:01 +0100

    fix formatting

--- custom_components/ikea_ansluta/light/ikea_ansluta_light.h
+++ custom_components/ikea_ansluta/light/ikea_ansluta_light.h
@@ -5,36 +5,33 @@
 #include "esphome/components/light/light_output.h"
 #include "esphome/core/helpers.h"
 
-namespace esphome
-{
-  namespace ikea_ansluta
-  {
+namespace esphome {
+namespace ikea_ansluta {
 
-    class IkeaAnslutaLight : public Component, public light::LightOutput
-    {
-    public:
-      void setup() override;
-      void loop() override;
-      void dump_config() override;
-      void set_radio(IkeaAnsluta *radio) { this->radio_ = radio; };
-      void set_remote_address(uint16_t address) { this->remote_address_ = address; };
-      void set_address(uint16_t address) { this->address_ = address; };
-      void set_pairing_enabled(bool allow) { this->pairing_enabled_ = allow; };
-      light::LightTraits get_traits() override;
-      void setup_state(light::LightState *state) override;
-      void write_state(light::LightState *state) override;
+class IkeaAnslutaLight : public Component, public light::LightOutput {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  void set_radio(IkeaAnsluta *radio) { this->radio_ = radio; };
+  void set_remote_address(uint16_t address) { this->remote_address_ = address; };
+  void set_address(uint16_t address) { this->address_ = address; };
+  void set_pairing_enabled(bool allow) { this->pairing_enabled_ = allow; };
+  light::LightTraits get_traits() override;
+  void setup_state(light::LightState *state) override;
+  void write_state(light::LightState *state) override;
 
-    protected:
-      IkeaAnsluta *radio_;
-      bool remote_pressed_ = false;
-      uint16_t remote_address_{};
-      optional<uint16_t> address_{};
-      bool pairing_enabled_{false};
-      uint32_t send_pairing_command_at_{0};
-      light::LightState *state_{nullptr};
-      void handle_remote_command_(IkeaAnslutaCommand command);
-      void send_command_(IkeaAnslutaCommand command);
-    };
+ protected:
+  IkeaAnsluta *radio_;
+  bool remote_pressed_ = false;
+  uint16_t remote_address_{};
+  optional<uint16_t> address_{};
+  bool pairing_enabled_{false};
+  uint32_t send_pairing_command_at_{0};
+  light::LightState *state_{nullptr};
+  void handle_remote_command_(IkeaAnslutaCommand command);
+  void send_command_(IkeaAnslutaCommand command);
+};
 
-  } // namespace ikea_ansluta
-} // namespace esphome
+}  // namespace ikea_ansluta
+}  // namespace esphome
